a57bcdcfa3c1d9b05337fb57a4de23d60e1fac34
LuckyGan <grx15002364225@hotmail.com>
2017-06-16 22:08:25 +0800

    fix some format problem

--- Singleton/Singleton_pthread_once.cc
+++ Singleton/Singleton_pthread_once.cc
@@ -3,9 +3,9 @@
  /// @author  zack(18357154046@163.com)
  /// @date    2017-06-15 19:18:21
 //调用pthread_once方法的单例模式
-//优点：1、利用pthread_once方法中注册的函数能在多线程环境下只被执行一次的特性，解决线程安全问题
-//		2、利用atexit回调函数，自动释放对象
-//		3、在未调用getInstance方法的情况下，在内存空间中不存在任何对象（解决了饱汉式的问题）
+//优点:1、利用pthread_once方法中注册的函数能在多线程环境下只被执行一次的特性，解决线程安全问题
+//     2、利用atexit回调函数，自动释放对象
+//     3、在未调用getInstance方法的情况下，在内存空间中不存在任何对象（解决了饱汉式的问题）
 #include <stdlib.h>
 #include <pthread.h>
 #include <iostream>
